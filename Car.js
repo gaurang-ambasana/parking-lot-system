15bf86e8070da9836813b1eaf7b96c5536f25a95
Gaurang Ambasana <gpambasana@gmail.com>
2022-04-15 02:28:41 +0530

    removed unnecessary method

--- Car.js
+++ Car.js
@@ -3,13 +3,6 @@ class Car {
     this.number = number;
     this.color = color;
   }
-
-  isSameCar(car1, car2) {
-    return (
-      car1.number.toLowerCase() === car2.number.toLowerCase() &&
-      car2.color.toLowerCase() === car2.color.toLowerCase()
-    );
-  }
 }
 
 exports.Car = Car;
