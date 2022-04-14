3f61c2ade6dea81cf7eb55a1cad8058238a7f680
Gaurang Ambasana <gpambasana@gmail.com>
2022-04-15 00:00:01 +0530

    added samecar method and exported

--- Car.js
+++ Car.js
@@ -1,6 +1,15 @@
-export class Car {
+class Car {
   constructor(number, color) {
     this.number = number;
     this.color = color;
   }
+
+  isSameCar(car1, car2) {
+    return (
+      car1.number.toLowerCase() === car2.number.toLowerCase() &&
+      car2.color.toLowerCase() === car2.color.toLowerCase()
+    );
+  }
 }
+
+exports.Car = Car;
