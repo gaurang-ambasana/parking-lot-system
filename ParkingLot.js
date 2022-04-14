7f8566652e625cfed741ef8b751b2f1003c5905e
Gaurang Ambasana <gpambasana@gmail.com>
2022-04-14 17:04:59 +0530

    added validation

--- ParkingLot.js
+++ ParkingLot.js
@@ -1,5 +1,6 @@
 export class ParkingLot {
   constructor(slots) {
+    if (slots <= 0) throw new Error(`Not possible :(`);
     this.maxSlots = slots;
     this.parkingSlots = new Array(slots).fill(null);
   }
